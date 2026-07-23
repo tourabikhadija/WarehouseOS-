0164bfcc89c107212ecf7e0e3414bad27ea9f3bf
khadija <italia.khadija2000@gmail.com>
2026-07-23 16:54:45 +0100

    crea la page home.css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,26 @@
+import Link from "next/link";
+import "@/style/Home.css";
+import Image from "next/image";
+
 export default function Home(){
     return (
-        <main>
+        <main className="home">
             <h1>WarehouseOS</h1>
+              <Image src="/images/bg.jpg" alt="img-cover" fill priority style={{
+                 zIndex: -1,}} 
+               />
+
+
+             <div className="btn">
+            <Link href="/login" className="login">
+            <button>Login</button>
+            </Link>
+            
+            <Link href="/register" className="register">
+            <button>Register</button>
+            </Link>
+             </div>
+            
         </main>
     );
 }
